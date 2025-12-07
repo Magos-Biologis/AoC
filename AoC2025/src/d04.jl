7df9e786cc11a0ace157c257b0a4aa5efa5682f9
Magos-Biologis <127683931+Magos-Biologis@users.noreply.github.com>
2025-12-07 12:56:20 +0100

    Moved and adapted from jupyter notebook

--- AoC2025/src/d04.jl
+++ AoC2025/src/d04.jl
@@ -5,9 +5,102 @@
 ### PART 1 
 ##############################################################################
 
+
+
+function initialize_paperrolls(str::AbstractString)
+
+    data = split(str, '\n')
+    grid = strings_to_grid(data)
+
+
+end
+
+
+# Compute the number of adjacent '@' around (i, j)
+function adjacent_at_entry(grid::Matrix{<:AbstractChar}, i::Int, j::Int)
+    N, M = size(grid)
+    count = 0
+
+    @inbounds for di in -1:1, dj in -1:1
+        (di == 0 && dj == 0) && continue
+
+        ii = i + di
+        jj = j + dj
+        if 1 <= ii <= N && 1 <= jj <= M
+            count += (grid[ii, jj] == '@')
+        end
+    end
+
+    return count
+end
+
+
+function mark_accessible(grid::Matrix{T}) where {T<:AbstractChar}
+    N, M = size(grid)
+    marked = copy(grid) 
+
+    @inbounds for i in 1:N, j in 1:M
+        if grid[i, j] == '@'
+            adj = adjacent_at_entry(grid, i, j)
+            if adj < 4
+                marked[i, j] = 'x'
+            end
+        end
+    end
+
+    return marked
+end
+
+
+# Count accessible '@' and produce marked grid (accessible '@' -> 'x')
+function count_accessible_rolls(grid::Matrix{T}) where {T<:AbstractChar}
+    N, M = size(grid)
+
+    marked = mark_accessible(grid) .== 'x'
+    accessible = sum( marked  )
+
+    return accessible, marked
+end
+
+
 ##############################################################################
 ### PART 2 
 ##############################################################################
+
+
+function step_round!(grid::AbstractMatrix{T}) where T<:AbstractChar
+    count_accessible, marked = count_accessible_rolls(grid)
+
+    # Avoid shadowing of `count` by using sum(M) to count trues in a BitMatrix.
+    removed_count = sum(marked)
+
+    # Remove all accessible in parallel
+    grid[marked] .= '.'
+
+    return removed_count, grid
+end
+
+
+function roll_removal(grid::AbstractMatrix{T}; maxrounds::Int = typemax(Int)) where T<:AbstractChar
+
+    per_counts = Int[]
+    total_removed = 0
+    rounds = 0
+
+    while rounds < maxrounds
+
+        removed_count, cur = step_round!(grid)
+        removed_count == 0 && break
+
+        push!(per_counts, removed_count)
+        total_removed += removed_count
+        rounds += 1
+    end
+
+    return per_counts, rounds
+end
+
+
 
 ##############################################################################
 ### Day's Function
@@ -15,13 +108,24 @@
 
 export Day04
 function Day04()
+    # using the input_data from from common.jl
     data = input_data(4) 
-    parsed_data = parse_data(data)
+    initial_array = initialize_paperrolls(data)
 
-    val1_array = check_range(parsed_data)
-    println("Answer 1: $(sum(val1_array))")
+    val1_array, marked1 = count_accessible_rolls(initial_array)
 
-    val2_array = new_check_range(parsed_data)
-    println("Answer 2: $(sum(val2_array))")
+    rows1 = [string(m...) for m in eachrow(marked1)]
+    whole_thing = string([ string(str, "\n") for str in rows1]...)
+
+    println("Rolls accessible: $(sum(val1_array))")
+    # println(whole_thing)
+
+
+
+    initial_array = initialize_paperrolls(data)
+    val2_array, rounds = roll_removal(initial_array)
+
+    println("Total rolls retrieved: $(sum(val2_array))")
+    #println(whole_thing)
 
 end
